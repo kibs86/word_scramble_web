ef2c3c881030c5de0089804ac046cbb84c7b582d
kibs86 <kibs086@gmail.com>
2016-11-21 15:40:35 -0500

    Create general web page
    - create first page that user will see when they hit the app

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,3 +5,8 @@
 
 // use require without a reference to ensure a file is bundled
 require('./example');
+
+// hide myAccount when page first loads
+$('#hide-myaccount').hide();
+
+// $('#hide-signup').hide();
