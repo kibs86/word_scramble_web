e78a6c9903918f8adae189f8c838f7975c1b9b42
kibs86 <kibs086@gmail.com>
2016-11-22 13:00:32 -0500

    convert game description to handlebars
    - change game description from direct input in index.html to handlebars
      template
    - update body font and font size
    - update font size of game title on main page

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,12 +3,16 @@
 // user require with a reference to bundle the file and use it in this file
 // var example = require('./example');
 const authEvents = require('./auth/events.js');
+const showDescriptionTemplate = require('./templates/game-description.handlebars');
 
 // use require without a reference to ensure a file is bundled
 // require('./example');
 
 // hide myAccount when page first loads
 $('#hide-myaccount').hide();
+
+// load the game description template
+$('#content').html(showDescriptionTemplate());
 
 // $('#hide-signup').hide();
 $(()=>{
