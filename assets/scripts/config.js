384e98385e6881d85fdaeda42157e31ee806b23f
kibs86 <kibs086@gmail.com>
2016-12-02 15:57:39 -0500

    update config to point to heroku
    - update config to point to heroku instead of localhost

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const config = {
-  host: 'http://localhost:4741',
-  //host: 'https://guarded-island-79443.herokuapp.com',
+  //host: 'http://localhost:4741',
+  host: 'https://guarded-island-79443.herokuapp.com',
 };
 
 module.exports = config;
